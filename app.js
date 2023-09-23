d6ede57ef1ad1c7eb83496cc62003f06d75f0198
leilenwinkler <leilenwinkler@gmail.com>
2023-09-23 15:49:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+    res.sendFile( path.join(__dirname, "/views/home.html") );
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en http://localhost:${port}`);
+});
